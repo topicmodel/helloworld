58edb89ea94c6b5a39a14e12c6b416ae5fbc2475
songkai <songkai@DESKTOP-7J4BL7K>
2019-03-18 20:30:31 +0800

    change

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class Helloworld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!我就随便改一改！！！");
 	}
 }
